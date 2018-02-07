7c494225ed65e4a7617f22df2815094441590801
RaetzkeMat <matthias.raetzke@zueblin.de>
2018-02-07 21:54:02 +0100

    create table in code behind

--- js/app.js
+++ js/app.js
@@ -49,17 +49,62 @@ $(function() {
             $(this).text(numMissed);
         });
     }
+    
+        // Check boxes, based on attendace records
+        $.each(attendance, function(name, days) {
+            var studentRow = $('tbody .name-col:contains("' + name + '")').parent('tr'),
+                dayChecks = $(studentRow).children('.attend-col').children('input');
 
-    // Check boxes, based on attendace records
-    $.each(attendance, function(name, days) {
-        var studentRow = $('tbody .name-col:contains("' + name + '")').parent('tr'),
-            dayChecks = $(studentRow).children('.attend-col').children('input');
-
-        dayChecks.each(function(i) {
-            $(this).prop('checked', days[i]);
+            dayChecks.each(function(i) {
+                $(this).prop('checked', days[i]);
+            });
         });
-    });
-
+        // Check boxes, based on attendace records
+    function buildTable(){
+        // my code to generate table
+        var tblBody = document.createElement("tbody");
+        $.each(attendance, function(name, days) {
+            var tr = document.createElement('tr');
+            tr.className ="student"
+            var td = document.createElement('td');
+            td.class  = "name-col";
+            td.innerHTML = name;
+            console.log(td);
+            tr.appendChild(td);
+            var dayscount = 0;
+            for(i = 0; i < days.length; i++) {
+                td = document.createElement('td');
+                td.className = "attend-col";
+    
+                var input = document.createElement("input");
+                input.type = "checkbox";
+                input.checked = days[i];
+                dayscount = dayscount + days[i];
+               
+                td.appendChild(input);
+                tr.appendChild(td);
+            }
+            attendance[name].dayscount = dayscount;
+            td = document.createElement('td');
+            td.className = "missed-col";
+            td.innerHTML = dayscount;
+            tr.appendChild(td);
+            
+    
+            tblBody.appendChild(tr);
+            // my code to generate table
+    
+            var studentRow = $('tbody .name-col:contains("' + name + '")').parent('tr'),
+                dayChecks = $(studentRow).children('.attend-col').children('input');
+    
+            dayChecks.each(function(i) {
+                $(this).prop('checked', days[i]);
+            });
+        });
+        var table = document.getElementById("my-table");
+        table.appendChild(tblBody);
+    }
+    
     // When a checkbox is clicked, update localStorage
     $allCheckboxes.on('click', function() {
         var studentRows = $('tbody .student'),
@@ -81,4 +126,5 @@ $(function() {
     });
 
     countMissing();
+    buildTable();
 }());
